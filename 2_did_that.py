4997643810b51c0d5d4f9d126706811e565a0e53
brian <feihcsim824@gmail.com>
2018-01-14 00:50:48 -0500

    integrate base conversions

--- 2_did_that.py
+++ 2_did_that.py
@@ -1,49 +1,72 @@
-def iteration(k):
-    def iteration_with_k(n):
-        digits = list(str(n).zfill(k)) # make sure the digits list is the correct length
+def to_base(b):
+    def to_base_b(n):
+        s = ""
+        while n:
+            s = str(n % b) + s
+            n /= b
+        return int(s)
+    return to_base_b
+
+
+def iteration(k, to_dec, to_base, filled_str):
+    def iteration_with_constants(n):
+        digits = list(filled_str(n)) # make sure the digits list is the correct length
         x_digits = sorted(digits, reverse=True)
         x = int(''.join(x_digits))
+        x_dec = to_dec(x)
         y_digits = reversed(x_digits)
         y = int(''.join(y_digits))
-        z = x - y
+        y_dec = to_dec(y)
+        z_dec = x_dec - y_dec
+        z = to_base(z_dec)
+        print '%i: %i(%i) - %i(%i) = %i(%i)' % (n, x_dec, x, y_dec, y, z_dec, z)
         return [x, y, z]
-    return iteration_with_k
+    return iteration_with_constants
+
 
 
 class Minion:
 
     def __init__(self, n, b):
-        self.cycle = {}
         self.n = n
         self.k = len(str(n))
-        self.iteration = iteration(self.k)
         self.b = b
-        self.iteration_count = 0
+        self.cycle = {}
+        self.filled_str = lambda x: str(x).zfill(self.k)
+        self.iteration = iteration(
+            k=self.k,
+            to_dec=lambda x: int(str(x), b),
+            to_base=to_base(b),
+            filled_str=self.filled_str
+        )
 
     def get_iterations(self, z=None):
         if z is None:
             z = self.n
 
-        z_str = str(z).zfill(self.k)
+        z_str = self.filled_str(z)
         if z_str not in self.cycle:
             xyz = self.iteration(z)
             new_z = xyz[2]
             if z == new_z:
-                # print "answer is 1 because the new z is the same as the last z, causing the cycle to get stuck on this value"
-                # print xyz
+                """
+                answer is 1 because the new z is the same as the last z,
+                causing the cycle to get stuck on this value
+                """
                 return 1
             self.cycle[z_str] = xyz
-            self.iteration_count += 1
             return self.get_iterations(new_z)
         else:
-            # print 'last', z_str
-            return self.iteration_count
+            return len(self.cycle)
 
 
 def answer(n, b):
     minion = Minion(n, b)
-    return minion.get_iterations()
+    ans = minion.get_iterations()
+    # print minion.cycle
+    return ans
 
 
 
-answer(1211, 10)
+# print answer(1211, 10)
+print answer(210022, 3)
