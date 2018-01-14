9833d5a5dfe13b3a11eba736ff37f957f66bd49d
brian <feihcsim824@gmail.com>
2018-01-14 02:14:41 -0500

    use OrderedDict to find length of cycle

--- 2_did_that.py
+++ 2_did_that.py
@@ -1,12 +1,16 @@
-def to_base(b):
-    def to_base_b(n):
-        s = ""
-        while n:
-            s = str(n % b) + s
-            n /= b
-        return int(s)
-    return to_base_b
+from collections import OrderedDict
 
+def to_base_b(b):
+    def n_to_base_b(n):
+        if n == 0:
+            return 0
+        digits = []
+        while n:
+            digits.append(int(n % b))
+            n /= b
+        ret = int(''.join(map(str, digits[::-1])))
+        return ret
+    return n_to_base_b
 
 def iteration(k, to_dec, to_base, filled_str):
     def iteration_with_constants(n):
@@ -19,7 +23,7 @@ def iteration(k, to_dec, to_base, filled_str):
         y_dec = to_dec(y)
         z_dec = x_dec - y_dec
         z = to_base(z_dec)
-        print '%i: %i(%i) - %i(%i) = %i(%i)' % (n, x_dec, x, y_dec, y, z_dec, z)
+        # print '%i: %i(%i) - %i(%i) = %i(%i)' % (n, x_dec, x, y_dec, y, z_dec, z)
         return [x, y, z]
     return iteration_with_constants
 
@@ -31,12 +35,12 @@ class Minion:
         self.n = n
         self.k = len(str(n))
         self.b = b
-        self.cycle = {}
+        self.cycle = OrderedDict()
         self.filled_str = lambda x: str(x).zfill(self.k)
         self.iteration = iteration(
             k=self.k,
             to_dec=lambda x: int(str(x), b),
-            to_base=to_base(b),
+            to_base=to_base_b(b),
             filled_str=self.filled_str
         )
 
@@ -48,7 +52,7 @@ class Minion:
         if z_str not in self.cycle:
             xyz = self.iteration(z)
             new_z = xyz[2]
-            if z == new_z:
+            if z == new_z or z == 0:
                 """
                 answer is 1 because the new z is the same as the last z,
                 causing the cycle to get stuck on this value
@@ -57,7 +61,7 @@ class Minion:
             self.cycle[z_str] = xyz
             return self.get_iterations(new_z)
         else:
-            return len(self.cycle)
+            return len(self.cycle) - self.cycle.keys().index(z_str)
 
 
 def answer(n, b):
@@ -67,6 +71,6 @@ def answer(n, b):
     return ans
 
 
-
-# print answer(1211, 10)
-print answer(210022, 3)
+# answer(0, 2)
+# answer(1211, 10)
+# answer(210022, 3)
