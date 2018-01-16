89615e517b1bd8434119643378c50a4b9a7b711f
brian <feihcsim824@gmail.com>
2018-01-15 19:43:20 -0500

    extending accounted scenarios and test cases

--- 2b_pass/index.py
+++ 2b_pass/index.py
@@ -10,6 +10,9 @@ def reduce_to_digits_filtered_by_remainder(accumulator, digit):
     return accumulator
 
 def convert_ascending_list_to_descending_int(sequence):
+    if not len(sequence):
+        return 0
+
     return int(''.join(map(str, list(reversed(sequence)))))
 
 def insort_into_accumulator(accumulator, item):
@@ -38,56 +41,75 @@ allows for partial function applications
 def compile_answer(first_to_keep):
     insort_into_first = insort_list(first_to_keep) # setup for insorting the second round of digits to keep
     # 2. provide the second list of the digits to keep (digits with a remainder of which all members are to be kept)
-    def insort_second(second_to_keep):
+    def compile_answer2(second_to_keep):
         second = insort_into_first(second_to_keep) # second = sorted(first_to_keep + second_to_keep)
         insort_into_second = insort_list(second)
         # 3. provide the third list of digits (some of these digits (at most 2) will be discarded
-        def insort_third(third_to_discard_from):
+        def compile_answer3a(third_to_discard_from):
             # 4. provide the list of digits to discard from third_to_discard_from
-            def discard_from_third(digits_to_discard):
+            def compile_answer3b(digits_to_discard):
                 third_to_keep = reduce(
                     discard_digit_from_accumulator,
                     digits_to_discard,
                     third_to_discard_from
                 )
                 third = insort_into_second(third_to_keep) # third = sorted(second_to_keep + third_to_keep)
-                return convert_ascending_list_to_descending_int(third)
-            return discard_from_third
-        return insort_third
-    return insort_second
+                le_answer = convert_ascending_list_to_descending_int(third)
+                # print le_answer
+                return le_answer
+            return compile_answer3b
+        return compile_answer3a
+    return compile_answer2
 
 
-# def answer(digits):
-#     digits = sorted(digits)
-#     digits_filtered_by_remainder = reduce(
-#         reduce_to_digits_filtered_by_remainder,
-#         digits,
-#         [
-#             [], # multiples of 3
-#             [], # digits with remainder 1
-#             []  # digits with remainder 2
-#         ]
-#     )
+def answer(digits):
+    digits = sorted(digits)
+    digits_by_remainder = reduce(
+        reduce_to_digits_filtered_by_remainder,
+        digits,
+        [
+            [], # digit % 3 == 0
+            [], # digit % 3 == 1
+            []  # digit % 3 == 2
+        ]
+    )
 
-#     # we only care about digits that aren't multiples of 3
-#     digits_sum = sum(digits_filtered_by_remainder[1]) + sum(digits_filtered_by_remainder[2])
-#     digits_sum_remainder = digits_sum % 3
+    digits0 = digits_by_remainder[0]
+    digits1 = digits_by_remainder[1]
+    digits2 = digits_by_remainder[2]
 
-#     """
-#     we just need the sum of the removable digits
-#     (ie the digits that are not multiples of 3)
-#     to be a multiple of 3. if the sum isn't so,
-#     remove the smallest number(s)
-#     """
+    # we only care about digits that aren't multiples of 3
+    digits_sum = sum(digits1) + sum(digits2)
+    digits_sum_remainder = digits_sum % 3
 
-#     if digits_sum_remainder == 0:
-#         return convert_sequence_to_int(digits)
-#     elif digits_sum_remainder == 1:
-#         remainder_1s = digits_filtered_by_remainder[1]
-#         if len(digits_filtered_by_remainder[1]) >= 1:
-#             insort_digits = insort_digits_then_convert_to_int(digits_filtered_by_remainder[0] + digits_filtered_by_remainder[2])
-#             return insort_digits([remainder_1s[0]])
+    """
+    we just need the sum of the removable digits
+    (ie the digits that are not multiples of 3)
+    to be a multiple of 3. if the sum isn't so, remove
+    the smallest and smallest number of digit(s)
+    """
 
-
-
-#     return int(''.join(map(str, digits)))
+    if digits_sum_remainder == 0:
+        return convert_ascending_list_to_descending_int(digits)
+    
+    compile_answer2 = compile_answer(digits0)
+    if digits_sum_remainder == 1:
+        if len(digits1) >= 1:
+            compile_answer3a = compile_answer2(digits2)
+            compile_answer3b = compile_answer3a(digits1)
+            return compile_answer3b(digits1[:1])
+        if len(digits2) >= 2:
+            compile_answer3a = compile_answer2(digits1)
+            compile_answer3b = compile_answer3a(digits2)
+            return compile_answer3b(digits2[:2])
+    if digits_sum_remainder == 2:
+        if len(digits2) >= 1:
+            compile_answer3a = compile_answer2(digits1)
+            compile_answer3b = compile_answer3a(digits2)
+            return compile_answer3b(digits2[:1])
+        if len(digits1) >= 2:
+            compile_answer3a = compile_answer2(digits2)
+            compile_answer3b = compile_answer3a(digits1)
+            return compile_answer3b(digits1[:2])
+    
+    return 0
--- 2b_pass/test.py
+++ 2b_pass/test.py
@@ -3,17 +3,47 @@ from index import answer
 
 class TestCodes(unittest.TestCase):
 
-    def test_normal(self):
-        # self.assertEqual(answer([3, 1, 4, 1]), 4311)
-        self.assertEqual(answer([3, 1, 4, 1, 5, 8]), 85431)
+    # given tests with confirmed answers
+    def test_given(self):
+        self.assertEqual(answer([3, 1, 4, 1]), 4311)
         # self.assertEqual(answer([3, 1, 4, 1, 5, 9]), 94311)
-        # self.assertEqual(answer([6, 2, 4, 1]), 642)
-        # self.assertEqual(answer([6, 2, 4, 2]), 642)
-        # self.assertEqual(answer([6, 2, 4, 1, 1]), 642)
-        # self.assertEqual(answer([6, 3, 4, 7]), 63)
-        # self.assertEqual(answer([6, 3, 8, 8, 2]), 63882)
-        # self.assertEqual(answer([0]), 0)
-        # self.assertEqual(answer([2]), 0)
+
+    # sum of digits has remainder 1
+    def test_sum_has_remainder_1(self):
+        
+        # remove 1 digit with a remainder 1
+        self.assertEqual(answer([3, 1, 4, 1, 5, 8]), 85431)
+        self.assertEqual(answer([6, 2, 4, 1]), 642)
+
+        # no digits with remainder 1, so discard two digits with remainder 2
+        self.assertEqual(answer([6, 2, 2]), 6)
+        self.assertEqual(answer([8, 8]), 0)
+
+        # not a single digit with remainder 1 and no two digits with remainder 2, so answer is 0
+        self.assertEqual(answer([2]), 0)
+        self.assertEqual(answer([5]), 0)
+        self.assertEqual(answer([8]), 0)
+
+    # sum of digits has remainder 2
+    def test_sum_has_remainder_2(self):
+        
+        # remove 1 digit with remainder 2
+        self.assertEqual(answer([6, 2, 4, 2]), 642)
+        
+        # remove 2 digits with remainder 1
+        self.assertEqual(answer([6, 4, 1, 1, 7]), 7641)
+
+        # not a single digit with remainder 2 and no two digits with remainder 1, so answer is 0
+        self.assertEqual(answer([1]), 0)
+        self.assertEqual(answer([4]), 0)
+        self.assertEqual(answer([7]), 0)
+        
+
+    # def test_custom(self):
+    #     # self.assertEqual(answer([6, 3, 4, 7]), 63)
+    #     # self.assertEqual(answer([6, 3, 8, 8, 2]), 63882)
+    #     # self.assertEqual(answer([0]), 0)
+    #     # self.assertEqual(answer([2]), 0)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
